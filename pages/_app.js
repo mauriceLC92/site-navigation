4265955118c80a857f8e2fb0dd76337c156adad4
Maurice <mauricelecordier@gmail.com>
2020-07-05 16:28:47 +0200

    added vercel top navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
-import '../css/tailwind.css'
-import '../css/tailwind.css'
-import Head from 'next/head'
+import '../css/tailwind.css';
+import '../css/tailwind.css';
+import Head from 'next/head';
 
-export default function App ({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="stylesheet" href="https://rsms.me/inter.inter.css" />
-      </Head>
+export default function App({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <link rel="stylesheet" href="https://rsms.me/inter.inter.css" />
+            </Head>
 
-      <Component {...pageProps} />
-    </>
-  )
+            <Component {...pageProps} />
+        </>
+    );
 }
--- pages/vercel.tsx
+++ pages/vercel.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
 const Vercel = () => {
-  return (
-    <div className="">
-      hello world
-    </div>
-  )
-}
+    return <div className="">hello world</div>;
+};
 export default Vercel;
