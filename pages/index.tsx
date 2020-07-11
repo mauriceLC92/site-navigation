d472735737df86ead25f7667ef0ebb3c882777b4
Maurice <mauricelecordier@gmail.com>
2020-07-11 12:04:57 +0200

    WIP - auth0 header in place

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,26 @@ import { Vercel } from '../components/Vercel';
 import { Github } from '../components/Github';
 import { NetLify } from '../components/Netlify';
 import { WashingtonPost } from '../components/WashingtonPost';
+import { Auth0Header } from '../components/Auth0';
 
 export default function Home() {
     return (
         <div className="space-y-6 bg-orange-400">
+            <WashingtonPost />
             <Vercel />
             <Github />
             <NetLify />
-            <WashingtonPost />
+            <Auth0 />
         </div>
     );
 }
+
+const Auth0 = () => {
+    return (
+        <div className="bg-white">
+            <div className="bg-white max-w-6xl mx-auto">
+                <Auth0Header />
+            </div>
+        </div>
+    );
+};
